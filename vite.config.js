f6d12843ab1407b7b55ce7764a711370da03a144
KevinMKW <mkwkvn@gmail.com>
2023-12-18 13:46:34 +0300

    Remove Tailwind configurations and add bootstrap

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from 'tailwindcss'
-import withMaterialTailwind from './tailwind.config'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-    react(),
-    tailwindcss(withMaterialTailwind)
+    react()
   ],
 })
