22ea9db6ec0802d60ef6f6a341b855747d04e9b3
KevinMKW <mkwkvn@gmail.com>
2023-12-20 17:27:56 +0300

    Add styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,13 +5,13 @@ import { Navbar, Nav, Container, Button } from 'react-bootstrap';
 const NavBar = () => {
   return (
     <>
-      <Navbar style={{ borderBottom: '2px solid black' }}>
+      <Navbar style={{ borderBottom: '2px solid black', marginBottom: '3rem' }}>
         <Container>
           <Navbar.Brand><Link className='nav-link' to="/">Brills</Link></Navbar.Brand>
           <Nav className="nav-list ms-auto">
             <Nav.Link><Link className='nav-link' to="/dashboard">Dashboard</Link></Nav.Link>
             <Nav.Link><Link className='nav-link' to="/profile">Account</Link></Nav.Link>
-            <Button><Link className='nav-link' to="/signup">Signup</Link></Button>
+            <Button className='align-self-center py-0'><Link className='nav-link' to="/signup">Signup</Link></Button>
           </Nav>
         </Container>
       </Navbar>
