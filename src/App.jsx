29ec1942ae87bd70d06aa7a8c9da9ec7c8b04d68
KevinMKW <mkwkvn@gmail.com>
2023-12-20 17:22:59 +0300

    Update the routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,9 @@ import { Routes, Route } from 'react-router-dom'
  import Home from "./components/Home/Home.jsx";
 import Dashboard from "./components/Dashboard/Dashboard.jsx"
 import Account from "./components/Account/Account.jsx"
-import AccountForm from "./components/Account/AccountForm.jsx"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AddBill from './components/Bill/AddBill.jsx';
+import SignUp from './components/Account/AccountForm.jsx';
 
 function App() {
 
@@ -16,7 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/profile" element={<Account />} />
-          <Route path="/signup" element={<AccountForm />} />
+          <Route path="/signup" element={<SignUp />} />
           <Route path="/addbill" element={<AddBill />} />
        </Routes>
     </>
