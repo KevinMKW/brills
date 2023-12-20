51a58d5f70c97587f36f2e088b5f18610643dbfd
KevinMKW <mkwkvn@gmail.com>
2023-12-20 17:36:27 +0300

    Modify constant initialization error

--- src/constant.js
+++ src/constant.js
@@ -1,14 +1,14 @@
-export const h1 = {
-    fontsize: '2.5',
-    fontweight: 'bold',
-    color: colors.gray,
-}
-
 export const colors = {
     success: 'green',
     warning: 'yellow',
     danger: 'red',
     gray: '#000205',
+}
+
+export const h1 = {
+    fontsize: '2.5em',
+    fontweight: 'bold',
+    color: colors.gray,
 }
 
 export const p = {
