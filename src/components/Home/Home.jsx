789602018e3404321e1d0443f9c9495ee0f58f36
KevinMKW <mkwkvn@gmail.com>
2023-12-20 17:26:30 +0300

    Add constants in a constant's component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,23 +1,18 @@
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { p, colors } from "../../constant";
 
 const Home = () => {
-    const colors = {
-        success: 'green',
-        warning: 'yellow',
-        danger: 'red'
-    }
-
     return (
         <div className="mt-5">
             <Button><Link className='nav-link' to="/dashboard">Go to Dashboard</Link></Button>
-            <h1>
+            <h1 className="mt-5 text-h" style={{ fontSize: '2.5em', fontWeight: 'bold', color: '#000205'}}>
                 Brills is an app made to help you quickly calculate your disposable income.
             </h1>
-            <p>
+            <p style={{ fontSize: p.fontsize, color: p.gray}}>
                 You tell it how much money you earn each month, along with how much you spend—and on what—and it tells you how much you have left.
             </p>
-            <p>
+            <p style={{ fontSize: p.fontsize, color: p.gray}}>
                 It tells you nice and clearly whether you’re <span style={{ color: colors.success }}>safe</span>, <span style={{ color: 'yellow' }}>at risk</span>, or spending <span style={{ color: 'red' }}>too much</span>.
             </p>
         </div>
---
+++ src/constant.js
@@ -0,0 +1,17 @@
+export const h1 = {
+    fontsize: '2.5',
+    fontweight: 'bold',
+    color: colors.gray,
+}
+
+export const colors = {
+    success: 'green',
+    warning: 'yellow',
+    danger: 'red',
+    gray: '#000205',
+}
+
+export const p = {
+    fontsize: '1.5em',
+    color: colors.gray
+}
