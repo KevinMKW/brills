0e5d4e94efd8b4532cdecfec158f2809629b18af
KevinMKW <mkwkvn@gmail.com>
2023-12-20 17:28:36 +0300

    update content

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,14 +1,21 @@
 import { Link } from "react-router-dom";
 import { Button } from "react-bootstrap";
+import { p } from "../../constant";
 
 const Dashboard = () => {
     return (
         <div>
             <div className="d-flex justify-content-between">
-                <h1>Dashboard</h1>
-                <Button><Link className='nav-link' to="/addbill">Add Bill</Link></Button>    
+                <h1 className=" text-bold">Dashboard</h1>
+                <Button className="align-self-center"><Link className='nav-link' to="/addbill">Add Bill</Link></Button>    
             </div> 
-
+            <p style={{ fontSize: p.fontsize, color: p.color }}>It looks like you still need to add your income. <Link style={{ color: p.color }} to="/profile">Edit your account</Link> to complete setup.</p>
+            <div className="card">
+                <div className="card-body text-center">
+                    <h2>No bills to show</h2>
+                    <p style={{ fontSize: p.fontsize, color: p.color }}>You&apos;ve got  no bills to pay at the moment. <Link style={{ color: p.color }} to="/addbill">Add One</Link> if you need to!</p>
+                </div>
+            </div>
         </div>
     );
 }
